d437ed9f1e9a827827974f73f0e05262ed24f5c6
Ryan Cathcart <ryanwcathcart@gmail.com>
2022-07-02 23:46:06 -0400

    Add basic axios package functionality in agent.ts

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,3 +1,22 @@
-export interface Test {
+import axios, { AxiosResponse } from 'axios';
 
-}
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+
+const responseBody = (response: AxiosResponse) => response.data;
+
+const requests = {
+  get: (url: string, params?: URLSearchParams) => axios.get(url, { params }).then(responseBody),
+  post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
+};
+
+const Account = {
+  login: (values: any) => requests.post('account/login', values),
+  register: (values: any) => requests.post('account/register', values),
+  currentUser: () => requests.get('account/currentUser'),
+};
+
+const agent = {
+  Account,
+};
+
+export default agent;
